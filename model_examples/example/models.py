0129256efb457151075988bc53a8b2f4766ec589
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-06-08 22:29:39 -0500

    Creando str para poder hacer consultas

--- model_examples/example/models.py
+++ model_examples/example/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Lenguage(models.Model):
     name = models.CharField(max_length=10)
     
+    def __str__(self):
+        return self.name
+    
 class Framework(models.Model):
     name = models.CharField(max_length=10)
     lenguage = models.ForeignKey(Lenguage, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
