d4463749eb4928dbd72e8f8a079747f77679bbcd
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-06-08 23:33:15 -0500

    Creando views para la impresion de pdf

--- model_examples/example/views.py
+++ model_examples/example/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world!")
+
+def generar_pdf(request):
+    # Obtener la plantilla HTML
+    template_path = 'plantilla_pdf.html'
+    context = {'some_data': 'Este es un ejemplo'}
+
+    # Renderizar la plantilla con el contexto
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # Crear una respuesta HTTP con el content_type como 'application/pdf'
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="reporte.pdf"'
+
+    # Convertir el HTML a PDF
+    pisa_status = pisa.CreatePDF(html, dest=response)
+
+    # Verificar si hubo algún error durante la conversión
+    if pisa_status.err:
+        return HttpResponse('Hubo un error al generar el PDF', status=500)
+
+    return response
