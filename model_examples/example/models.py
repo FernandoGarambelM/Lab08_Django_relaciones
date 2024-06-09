006d722eafaedcf4f9cce60a6831dd5ee52d2321
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-06-08 22:25:48 -0500

    Creamos los modelos para trabajar las relaciones

--- model_examples/example/models.py
+++ model_examples/example/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Lenguage(models.Model):
+    name = models.CharField(max_length=10)
+    
+class Framework(models.Model):
+    name = models.CharField(max_length=10)
+    lenguage = models.ForeignKey(Lenguage, on_delete=models.CASCADE)
