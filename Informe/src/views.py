28f19519314d6bb1d15da122747c7b2cf35a94a8
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-06-11 23:42:05 -0500

    Informe, pdf y envio de correos con django

--- Informe/src/views.py
+++ Informe/src/views.py
@@ -1,40 +1,108 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Destination
-from .forms import DestinosTuristicosForm
-# Create your views here.
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import get_template
+from django.shortcuts import render
+from xhtml2pdf import pisa
+from .models import Lenguage, Framework, Movie, Character
+from django.core.mail import EmailMessage
+from io import BytesIO
 
 def index(request):
-   
-    dests = Destination.objects.all()
+    return HttpResponse("Hello, world!")
 
-    return render(request, "index.html", {'dests': dests})
+def generar_pdf(request):
+    template_path = 'plantilla_pdf.html'
+    context = {
+        'lenguages': Lenguage.objects.all(),
+        'frameworks': Framework.objects.all(),
+        'movies': Movie.objects.all(),
+        'characters': Character.objects.all()
+    }
 
-def gestionar_destinos(request, id=None):
-    if request.method == 'POST':
-        if id:
-            destino = get_object_or_404(Destination, id=id)
-            form = DestinosTuristicosForm(request.POST, request.FILES, instance=destino)
-            if 'modificar' in request.POST:
-                if form.is_valid():
-                    form.save()
-            elif 'eliminar' in request.POST:
-                destino.delete()
-        else:
-            form = DestinosTuristicosForm(request.POST, request.FILES)
-            if form.is_valid():
-                form.save()
-        return redirect('gestionar_destinos')
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="datos_modelos.pdf"'
     
-    else:
-        if id:
-            destino = get_object_or_404(Destination, id=id)
-            form = DestinosTuristicosForm(instance=destino)
-        else:
-            form = DestinosTuristicosForm()
-        
-    destinos = Destination.objects.all()
-    return render(request, 'gestionar_destinos.html', {
-        'form': form,
-        'destinos': destinos,
-        'id': id,
-    })
+    template = get_template(template_path)
+    html = template.render(context)
+    
+    pisa_status = pisa.CreatePDF(html, dest=response)
+    
+    if pisa_status.err:
+        return HttpResponse('Hubo un error al generar el PDF: %s' % pisa_status.err)
+    return response
+
+def enviar_correo(request):
+    context = {
+        'lenguages': Lenguage.objects.all(),
+        'frameworks': Framework.objects.all(),
+        'movies': Movie.objects.all(),
+        'characters': Character.objects.all()
+    }
+
+    if request.method == 'POST':
+        asunto = request.POST['asunto']
+        mensaje = request.POST['mensaje']
+        destinatario = request.POST['destinatario']
+        remitente = 'fgarambel@unsa.edu.pe'  
+
+        if 'send_plain' in request.POST:
+            try:
+                send_mail(asunto, mensaje, remitente, [destinatario])
+                return HttpResponse('Correo enviado exitosamente')
+            except Exception as e:
+                return HttpResponse(f'Error al enviar el correo: {str(e)}')
+
+        elif 'send_pdf' in request.POST:
+            template_path = 'plantilla_pdf.html'
+            template = get_template(template_path)
+            html = template.render(context)
+            
+            result = BytesIO()
+            pisa_status = pisa.CreatePDF(html, dest=result)
+            
+            if pisa_status.err:
+                return HttpResponse('Hubo un error al generar el PDF: %s' % pisa_status.err)
+            
+            email = EmailMessage(
+                asunto,
+                mensaje,
+                remitente,
+                [destinatario]
+            )
+            
+            email.attach('datos_modelos.pdf', result.getvalue(), 'application/pdf')
+            email.send()
+            
+            return HttpResponse('Correo con PDF enviado exitosamente')
+    
+    return render(request, 'enviar_correo.html', context)
+
+def enviar_pdf_por_correo(request):
+    template_path = 'plantilla_pdf.html'
+    context = {
+        'lenguages': Lenguage.objects.all(),
+        'frameworks': Framework.objects.all(),
+        'movies': Movie.objects.all(),
+        'characters': Character.objects.all()
+    }
+
+    template = get_template(template_path)
+    html = template.render(context)
+    
+    result = BytesIO()
+    pisa_status = pisa.CreatePDF(html, dest=result)
+    
+    if pisa_status.err:
+        return HttpResponse('Hubo un error al generar el PDF: %s' % pisa_status.err)
+    
+    email = EmailMessage(
+        'Datos de Modelos',
+        'Adjunto encontraras el PDF con los datos de los modelos.',
+        'tu_correo@gmail.com',
+        ['destinatario@gmail.com']
+    )
+    
+    email.attach('datos_modelos.pdf', result.getvalue(), 'application/pdf')
+    email.send()
+    
+    return HttpResponse('Correo enviado exitosamente')
